8f69dfc2a2620c25b65b24022e51d72661bf42cc
janek <janek8282@gamil.com>
2024-01-29 22:34:52 +0100

    New console.logs

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 console.log('Siemka!!!')
 console.log('Siemka 2')
 console.log('Siemka 3');
+console.log('Siemka 4');
+console.log('Siemka 5');
+console.log('Siemka 6');
+console.log('Siemka 7');
+console.log('Siemka 8');
+
